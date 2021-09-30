9d23e6d10750f1d623be76eebda54051373e4b0a
Durgasri Sorra <durgasrisorra@gmail.com>
2021-10-01 00:06:02 +0530

    files added

---
+++ Day 1/Holiday.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_1_Csharp_Assignment
+{
+    class Holiday
+    {
+        public static void Main1()
+        {
+            int n;
+            Console.WriteLine("Enter the Number:");
+            n = int.Parse(Console.ReadLine());
+            if (n >= 1 && n <= 5)
+            {
+                Console.WriteLine("It is a Working Day");
+            }
+            else if (n > 5 && n <= 7)
+            {
+                Console.WriteLine("It is a Holiday");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Number");
+            }
+        }
+    }
+}
