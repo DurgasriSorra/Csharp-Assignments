b3c1856af0de7211cb3141a25dcdc6a3c1fafd4a
Durgasri Sorra <durgasrisorra@gmail.com>
2021-09-30 23:43:02 +0530

    created

---
+++ Day 3/BankAcc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2
+{
+    
+        class BankAcc
+        {
+            double balance;
+
+            public BankAcc()
+            {
+                balance = 500;
+            }
+            public BankAcc(double balance)
+            {
+
+
+                this.balance = balance;
+            }
+
+
+
+            public double getBalance()
+            {
+                return balance;
+            }
+
+
+        }
+        class BankTest
+        {
+            public static void Main(string[] args)
+            {
+                BankAcc b1 = new BankAcc();
+                Console.WriteLine("default balance=" + b1.getBalance());
+                BankAcc b2 = new BankAcc(1000);
+                Console.WriteLine(" balance=" + b2.getBalance());
+
+            }
+        }
+    }
